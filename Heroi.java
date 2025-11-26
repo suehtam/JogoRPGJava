aa6c846dc67702fca9d4c60e12ab3cbc3f2921b5
Matheus Rodrigues Alexandre e Silva <matheus4rodrigues@hotmail.com>
2025-11-26 19:46:07 -0300

    Create Heroi.java

---
+++ Heroi.java
@@ -0,0 +1,31 @@
+public interface Heroi
+{
+    void atacar()//Exibe uma mensagem de ataque básico
+    {
+        System.out.println("Atacando");
+    }
+    void defender()//Exibe uma mensagem de defesa
+    {
+        System.out.println("Defender");
+    }
+    void executarUltimata()//Exibe a habilidade especial única da classe
+    {
+        System.out.println("Executando Ultimata");
+    }
+    void apresentar()//Mostra os dados do herói
+    {
+        System.out.println("Apresentando");
+    }
+    void lutar(HeroiAdversario)
+    {
+        System.out.println()
+    }
+    String getNome()//Retorna o nome do herói
+    {
+
+    }
+    int getNivel()//Retorna o nível de poder
+    {
+
+    }
+}
