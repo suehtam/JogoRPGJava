f3a594268bb3d0ff701b55fa0b59f699326a029a
Matheus Rodrigues Alexandre e Silva <matheus4rodrigues@hotmail.com>
2025-11-26 20:19:58 -0300

    Refactor Heroi interface to remove method bodies

--- Heroi.java
+++ Heroi.java
@@ -1,31 +1,10 @@
 public interface Heroi
 {
-    void atacar()//Exibe uma mensagem de ataque básico
-    {
-        System.out.println("Atacando");
-    }
-    void defender()//Exibe uma mensagem de defesa
-    {
-        System.out.println("Defender");
-    }
-    void executarUltimata()//Exibe a habilidade especial única da classe
-    {
-        System.out.println("Executando Ultimata");
-    }
-    void apresentar()//Mostra os dados do herói
-    {
-        System.out.println("Apresentando");
-    }
-    void lutar(HeroiAdversario)
-    {
-        System.out.println()
-    }
-    String getNome()//Retorna o nome do herói
-    {
-
-    }
-    int getNivel()//Retorna o nível de poder
-    {
-
-    }
+    void atacar();
+    void defender();
+    void executarUltimata();
+    void apresentar();
+    void lutar(Heroi adversario);
+    String getNome();
+    int getNivel();
 }
