3897b25c517cc1f3fa1ab85d57f6bc729a348385
Matheus Rodrigues Alexandre e Silva <matheus4rodrigues@hotmail.com>
2025-11-27 15:25:56 -0300

    Implement HeroiDeAgilidade class methods

--- HeroiDeAgilidade.java
+++ HeroiDeAgilidade.java
@@ -1,4 +1,22 @@
 public class HeroiDeAgilidade extends Personagem
 {
-
+    public HeroiDeAgilidade(String nome, int nivel)
+    {
+        super(nome, nivel, "Herói de Agilidade");
+    }
+    @Override
+    public void atacar()
+    {
+        System.out.println(nome + ": Ataca com movimentos rápidos e precisos!");
+    }
+    @Override
+    public void defender()
+    {
+        System.out.println(nome + ": Se esquiva com reflexos incríveis!");
+    }
+    @Override
+    public void executarUltimata()
+    {
+        System.out.println(nome + ": Libera um Tornado de Velocidade que confunde o adversário!");
+    }
 }
