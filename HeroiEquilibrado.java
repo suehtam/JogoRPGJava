c77bd6c0fa6e2e3b8fae89eeb56cfb29ce527128
Matheus Rodrigues Alexandre e Silva <matheus4rodrigues@hotmail.com>
2025-11-27 15:27:12 -0300

    Implement HeroiEquilibrado class with actions

--- HeroiEquilibrado.java
+++ HeroiEquilibrado.java
@@ -1,4 +1,22 @@
 public class HeroiEquilibrado extends Personagem
 {
-
+    public HeroiEquilibrado(String nome, int nivel)
+    {
+        super(nome, nivel, "Herói Equilibrado");
+    }
+    @Override
+    public void atacar()
+    {
+        System.out.println(nome + ": Ataca com uma combinação de força e agilidade!");
+    }
+    @Override
+    public void defender()
+    {
+        System.out.println(nome + ": Defende com disciplina e estratégia!");
+    }
+    @Override
+    public void executarUltimata()
+    {
+        System.out.println(nome + ": Canaliza o Poder Harmonioso, equilibrando ataque e defesa!");
+    }
 }
